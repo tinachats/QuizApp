f2b044ac71c681e7e3e295ea926e60582a193b37
Tinashe Chaterera <marshallchaterera@gmail.com>
2024-08-23 18:07:32 +0200

    Controllers and Models for Questions and Answers

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-between my-5">
+            <div class="col col-md-6">
+                <h1 class="display-4 fw-bolder">
+                    Daily Quiz, Daily <br>Bonus-Play Today!
+                </h1>
+                <p class="text-muted lead">
+                    QuizApp is the daily Trivia and quiz playing platform.
+                    It brings <br>some exciting surprises every day.
+                </p>
+            </div>
+            <div class="col col-md-6"></div>
+        </div>
+    </div>
+</template>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,12 +7,13 @@
         <title inertia>{{ config('app.name', 'QuizApp') }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Zain:wght@200;300;400;700;800;900&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
         @routes
-        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+        @vite(['resources/js/app.scss', 'resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
